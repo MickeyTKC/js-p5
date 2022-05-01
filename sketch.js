37091375b6b8ceae90d42bd15ba31ec73c12b3b7
MickeyTKC <tt51145505@gmail.com>
2022-05-01 22:41:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let audioElement;
-let slide;
+let bgImage;
+let song; 
+let fft;
 
 const time = [
     23,
@@ -13,22 +15,30 @@ const time = [
     235
 ];
 
-function setup() {
-  createCanvas(1280, 720);
+function preload(){
+  sound = loadSound('sound.mp3');
+}
 
-  audioElement = createAudio('sound.mp3');
-  audioElement.position(0, 720); 
-  audioElement.size(300); 
-  audioElement.autoplay(true);
-  audioElement.showControls(); 
-  
-  slide = 0;
+function setup() {
+  const cnv = createCanvas(1280, 720);
+  cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(0.5);
+  bgImage = loadImage('bg.png');
+  fft = new p5.FFT()
+  sound.loop();
 }
 
 function draw() {
-  background(220);
-  console.log(audioElement.time())
+  //background(bgImage);
+  displayBackground()
+  displayTitle()
+  //testing code
+  displayFrequency()
+}
 
+// Time handling 
+const timeTesting =()=>{
   if(audioElement.time() >= time[0]){
     background(time[0]);
   }
@@ -58,6 +68,52 @@ function draw() {
   }
 }
 
-const fadeIn =()=>{
+/** Function Toggle Play */
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
+}
 
+/** Function Display Background */
+const displayBackground = () =>{
+  const w = bgImage.width / 1.5;
+  const h = bgImage.height / 1.5;
+  image(bgImage,0,0,w, h); // create image to canvas
+}
+
+/** Function Display Title and Author */
+const displayTitle = () =>{
+  const fontSize = {title: 38,author: 24};
+  const fontColor = "#EEE"; //rgb(255, 255, 255)
+  const fontFamily = "Arial";
+  const position = {x:200,y:bgImage.height / 1.5 / 3}
+
+  fill(fontColor);
+  textFont(fontFamily);
+  select('canvas').elt.style.letterSpacing = "0.25rem"; // Font Space By CSS
+  textSize(fontSize.title);
+  text("Ninelie", position.x, position.y);
+  textSize(fontSize.author);
+  text("Aimer with chelly", position.x, position.y + 35);
+}
+
+/** Function Display Sound */
+const displayFrequency=()=>{
+  let spectrum = fft.analyze();
+  const slen = 700
+  const fwidth = (width - spectrum.length) / 2;
+  const fheight = bgImage.height / 1.5 / 2;
+  var space = 0;
+
+  console.log(spectrum)
+  stroke(255)
+  for (i=0;i<spectrum.length;i+=2){
+    const amp = spectrum[i];
+    const y = map(amp,0,1023,fheight,0)
+    if(i<700){line(i+fwidth+space,fheight,i+fwidth+space,y);}
+    space++;
+  }
 }
